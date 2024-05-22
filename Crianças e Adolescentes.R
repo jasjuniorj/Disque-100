861a8a202b178af82c416553c2c8329de4094148
José Alexandre <98750746+jasjuniorj@users.noreply.github.com>
2024-05-22 20:15:41 -0300

    Add files via upload

--- Crianças e Adolescentes.R
+++ Crianças e Adolescentes.R
@@ -196,4 +196,80 @@ LocBCA %>%
   theme_classic()+theme(legend.position = "bottom") 
 
 
+########################### Violência Sexual #####################################################
 
+
+BABUSO <-subset(BCA, grepl("Abuso Sexual|Violência Sexual|VIOLÊNCIA SEXUAL|ASSÉDIO SEXUAL|
+assédio sexual|Assédio Sexual|Estupro|estupro|Importunação Sexual|importunação sexual|
+abuso|Abuso|Abuso Sexual|abuso sexual|Exploração Sexual|exploração sexual|
+ESTUPRO|IMPORTUNAÇÃO SEXUAL|ABUSO|ABUSO SEXUAL|EXPLORAÇÃO SEXUAL", violacao))
+
+## Suspeito
+
+BABUSO <- BABUSO %>% 
+  mutate(rel_suspeito = case_when(
+    (relacao_suspeito=="Pai")|(relacao_suspeito=="PAI")|
+      (relacao_suspeito=="Avó")|
+      (relacao_suspeito=="Avô")|
+      (relacao_suspeito=="AVÔ(Ó)")|
+      (relacao_suspeito=="Cunhado (a)")|
+      (relacao_suspeito=="CUNHADO (A)")|(relacao_suspeito=="Enteado(a)")|
+      (relacao_suspeito=="ENTEADO(A)")|
+      (relacao_suspeito=="Familiares")|(relacao_suspeito=="Filho (a)")|
+      (relacao_suspeito=="FILHO (A)")|(relacao_suspeito=="FILHO(A)")|(relacao_suspeito=="Genro/Nora")|
+      (relacao_suspeito=="GENRO/NORA")|
+      (relacao_suspeito=="Irmão (ã)")|(relacao_suspeito=="IRMÃO (Ã)")|
+      (relacao_suspeito=="IRMÃO(Ã)")|(relacao_suspeito=="Madrasta")|(relacao_suspeito=="Mãe")|
+      (relacao_suspeito=="MÂE")|
+      (relacao_suspeito=="Neto(a)")|(relacao_suspeito=="NETO(A)")|(relacao_suspeito=="Padrasto")|
+      (relacao_suspeito=="PADRASTO/ MADRASTA")|(relacao_suspeito=="PADRASTO/MADRASTA")|
+      (relacao_suspeito=="PADRINHO/ MADRINHA")|
+      (relacao_suspeito=="Padrinho/Madrinha")|(relacao_suspeito=="PADRINHO/MADRINHA")|
+      (relacao_suspeito=="Pai")|(relacao_suspeito=="PAI")|
+      (relacao_suspeito=="PAI/ MÃE")|(relacao_suspeito=="Primo(a)")|
+      (relacao_suspeito=="PRIMO(A)")|(relacao_suspeito=="Sobrinho(a)")|
+      (relacao_suspeito=="SOBRINHO(A)")|(relacao_suspeito=="Sogro(a)")|
+      (relacao_suspeito=="SOGRO(A)")|(relacao_suspeito=="Tio (a)")|
+      (relacao_suspeito=="TIO (A)")|(relacao_suspeito=="TIO(A)")|
+      (relacao_suspeito=="TRISAVÔ(Ó)")~'Familiares/Amigos',
+    (relacao_suspeito=="COMPANHEIRO (A)")|
+      (relacao_suspeito=="COMPANHEIRO(A)")|
+      (relacao_suspeito=="Companheiro (a)")|
+      (relacao_suspeito=="COMPANHEIRO(A) DA MÃE/DO PAI")|
+      (relacao_suspeito=="MARIDO")|(relacao_suspeito=="Marido")|
+      (relacao_suspeito=="PADRASTO/MADRASTA")|
+      (relacao_suspeito=="Madrasta")|(relacao_suspeito=="MÃE")|
+      (relacao_suspeito=="Mãe")|(relacao_suspeito=="ESPOSA")|(relacao_suspeito=="ESPOSA(O)")|
+      (relacao_suspeito=="Esposa")|
+      (relacao_suspeito=="EX- NAMORADO(A)")|(relacao_suspeito=="Ex-Companheiro (a)")|
+      (relacao_suspeito=="EX-COMPANHEIRO (A)")|(relacao_suspeito=="Ex-Esposa")|
+      (relacao_suspeito=="EX-ESPOSA")|
+      (relacao_suspeito=="EX-ESPOSA(O)")|(relacao_suspeito=="Ex-Marido")|
+      (relacao_suspeito=="Ex-Marido")|(relacao_suspeito=="EX-MARIDO")|
+      (relacao_suspeito=="EX-NAMORADO (A)")|(relacao_suspeito=="EX-NAMORADO(A)")~'Familiares/Amigos', #Companheiro(a)/Ex-Companheiro(a)
+    (relacao_suspeito=="Amigo (a)")|(relacao_suspeito=="AMIGO (A)")|(relacao_suspeito=="AMIGO(A)")|
+      (relacao_suspeito=="AMIGO(A) DA FAMÍLIA")|(relacao_suspeito=="COLEGA DE TRABALHO (MESMO NÍVEL HIERÁRQUICO)")|
+      (relacao_suspeito=="MESMO NÍVEL HIERÁRQUICO")|(relacao_suspeito=="MORA NA MESMA RESIDÊNCIA MAS NÃO É FAMILIAR")|
+      (relacao_suspeito=="MOROU NA MESMA RESIDÊNCIA MAS NÃO É FAMILIAR")|
+      (relacao_suspeito=="PESSOA COM QUEM MANTÉM/MANTEVE CONVIVÊNCIA FAMILIAR")~"Familiares/Amigos", #Amigos/Conhecidos
+    (relacao_suspeito=="Vizinho (a)")|(relacao_suspeito=="VIZINHO (A)")|
+      (relacao_suspeito=="VIZINHO(A)")~"Outros", #Vizinhos
+    TRUE ~ 'Outros'
+  ))
+
+SUMBCA <- BABUSO %>% 
+  group_by(rel_suspeito,Ano) %>% 
+  summarise(valor=n())
+
+
+SUMBCA %>% 
+  ggplot(aes(x=Ano, y = valor/1000, color = rel_suspeito)) +
+  geom_line(size=1) +
+  scale_y_continuous(breaks = seq(0,50,10), limits = c(0,50))+
+  scale_x_continuous(breaks = seq(2011,2023, 1), limits = c(2011,2023))+
+  scale_color_manual(values = c("#FE9B2B", "#000000"))+
+  labs(title = "Denúncias de Violência Sexual de Direitos Contra Crianças e
+        Adolescentes por Relação com Suspeito (2011-2023) ", x = " ", color="Suspeitos",
+       y = "Mil", caption='Fonte: MDHC - Disque 100.
+       Elaboração do Autor.') +
+  theme_classic()+theme(legend.position = "bottom") 
